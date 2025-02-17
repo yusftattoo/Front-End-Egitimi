287aff31014a89c563039829773139ee7d07ebe2
yusuf kayabasi <yusuftattoo@gmail.com>
2025-02-17 23:57:33 +0300

    Decimal to Binary

--- 5-JS/EH/014-function-exmp-47.ders.js
+++ 5-JS/EH/014-function-exmp-47.ders.js
@@ -20,42 +20,51 @@
 
  //! **** Mukemmel Sayi Bulma Aplic. ****
 
-//  let number1 = Number(prompt("Lutfen bir sayi yazin."));
 
-//  function isPerfectNumber(number1){
+// let number1 = Number(prompt("Lütfen bir sayı yazın.")); //! Sayıyı Number'a çeviriyoruz.
+
+// function isPerfectNumber(number1) {
 //     let toplam1 = 0;
-//    
-//     for(let i1=1 ; i1 <= number1 / 2 ; i1++ ){ .
-//         if(number1 % i1 == 0){ // tam bolunuyorsa
+//          //! burada verilen i-i1 degerleri dinamik degerdir.
+//     for (let i1 = 1; i1 <= number1 / 2; i1++) {  //! 1 dahil edilmeli ve i1 <= number1/2 olmalı
+//         //! verilen number in yarisina kadar don, her seferinde birer arttiracak
+//         if (number1 % i1 == 0) {  //! Sayının bölenlerini topluyoruz.
 //             toplam1 += i1;
-//         } 
+//         }
 //     }
-//     toplam1+=1+number1;
 
-//     if(toplam1 == number1*2){
-//         console.log(number1 + "Mukemmel sayidir.");
-//     }else{
-//         console.log(number1 + "Mukemmel sayi degildir.");
+//     if (toplam1 == number1) {  //! Kendisi hariç bölenlerin toplamı kendisine eşit olmalı.
+//         console.log(number1 + " mükemmel sayıdır.");
+//     } else {
+//         console.log(number1 + " mükemmel sayı değildir.");
 //     }
-//  }
+// }
 
-let number1 = Number(prompt("Lütfen bir sayı yazın.")); // Sayıyı Number'a çeviriyoruz.
+// isPerfectNumber(number1); //! Fonksiyonu çağırıyoruz.
 
-function isPerfectNumber(number1) {
-    let toplam1 = 0;
-         //! burada verilen i-i1 degerleri dinamik degerdir.
-    for (let i1 = 1; i1 <= number1 / 2; i1++) {  //! 1 dahil edilmeli ve i1 <= number1/2 olmalı
-        //! verilen number in yarisina kadar don, her seferinde birer arttiracak
-        if (number1 % i1 == 0) {  //! Sayının bölenlerini topluyoruz.
-            toplam1 += i1;
+
+
+//! Decimal to Binary Uygulamasi
+
+convertDecimalToBinary(23);
+
+function convertDecimalToBinary(number){
+    let binary ="";
+    while(true){
+        binary += (number%2).toString();
+        number=Math.floor(number/2);
+        if(number == 1){
+            binary += 1;
+            break;
         }
     }
 
-    if (toplam1 == number1) {  // Kendisi hariç bölenlerin toplamı kendisine eşit olmalı.
-        console.log(number1 + " mükemmel sayıdır.");
-    } else {
-        console.log(number1 + " mükemmel sayı değildir.");
-    }
 }
 
-isPerfectNumber(number1); // Fonksiyonu çağırıyoruz.
+function reverse(binary){
+    let reverseBinary = "";
+    for(let i = binary.lenght-1 ; i>0 ; i--){
+        reverseBinary += binary.charAt(i);
+    }
+    return reverseBinary;
+    }
